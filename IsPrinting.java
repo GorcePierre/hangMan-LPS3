147fb5986d2645c513a1f63a03877fdf5cd719a1
CedricT33 <cedric.tressous@gmail.com>
2018-10-17 14:53:31 +0200

    optimisation IsPrinting

--- IsPrinting.java
+++ IsPrinting.java
@@ -22,6 +22,18 @@ public class IsPrinting {
       return editedText;
     }
 }
+
+//mon changement : remplacer la premiere class par celle ci et dans le main changer IsPrinting(0) par IsPrinting("title")
+public class IsPrinting(String word){
+  Hashtable<String, String> sentence = new Hashtable<String, String>();
+  sentence.put("title", "\n----------------------\n| WELCOME TO HANGMAN |\n----------------------\n");
+  sentence.put("userMaxLetter", "Entrez le nombre maximum de lettres");
+  sentence.put("playerOne", "Joueur1, entrez un mot: ");
+  sentence.put("playerTwo", "Joueur2, entrez une lettre: ");
+  System.out.println(sentence.get(word));
+  return sentence.get(word);
+}
+
 /**
  * Creating with <3 by LPS3 14/10/2018
  */
